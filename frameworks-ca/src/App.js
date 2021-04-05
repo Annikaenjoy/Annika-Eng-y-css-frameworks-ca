4ca1a4f016d5895391cd00782558d406455bee91
Annika Wassmo <annikaa@live.no>
2021-04-05 23:34:16 +0200

    First setup

---
+++ frameworks-ca/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Heading from '../src/components/Heading';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading content="We do YAY things"/>
+    </div>
+  );
+}
+
+export default App;
